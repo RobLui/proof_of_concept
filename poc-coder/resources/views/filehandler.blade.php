aaacb109d83612ad7999a577657e6f4edf05da4a
Robbert <robbertluit@hotmail.com>
2017-12-17 17:47:13 +0100

    adjustments large files

--- poc-coder/resources/views/filehandler.blade.php
+++ poc-coder/resources/views/filehandler.blade.php
@@ -8,27 +8,30 @@
 
         @if(!empty($data))
 
+            {{-- OPTIONS --}}
+            <div class="col-md-6 full-display full-height col-md-offset-3" id="options">
+                <h2 class="margin-top-3">OPTIONS</h2>
+            </div>
+
+            {{-- FILE CONTENTS --}}
+            <div class="col-md-6 full-display col-md-offset-3 full-height" id="raw-content">
+                <h2 class="margin-top-3">RAW CONTENT</h2>
+                <p class="margin-top-3 text-left block"> {{ $data }} </p>
+            </div>
+
+            {{-- OTHER --}}
+            <div class="col-md-6 full-display full-height col-md-offset-3">
+            </div>
 
             {{-- DETAILS FROM FILE CONTENTS --}}
             <div class="col-md-6 col-md-offset-3 margin-top-3 margin-bottom-3 full-height" id="statistics">
                 <h2 class="margin-top-3">STATISTICS</h2>
                 <ul class="list-group margin-top-3">
-                    <li class="col-md-12 list-group-item pink">Totaal aantal woorden: {{ $total }}</li>
+                    <li class="list-group-item pink">Totaal aantal woorden: {{ $total }}</li>
                     @foreach($eachWord as $item)
-                    <li class="col-md-4 list-group-item {{ $item->count > 2 ? 'green' : 'blue'  }}">{{ $item->word }} = <span>{{ $item->count }} keer</span></li>
+                        <li class="col-md-4 list-group-item {{ $item->count > 2 ? 'green' : 'blue'  }}">{{ $item->word }} = <span>{{ $item->count }} keer</span></li>
                     @endforeach
                 </ul>
-            </div>
-
-            {{-- FILE CONTENTS --}}
-            <div class="col-md-6 full-display col-md-offset-3 full-height" id="raw-content">
-                <h2 class="margin-top-3">RAW CONTENT:</h2>
-                <p class="margin-top-3 text-left"> {{ $data }} </p>
-            </div>
-
-            {{-- OTHER --}}
-            <div class="col-md-6 full-display full-height col-md-offset-3" id="other">
-                <h2 class="margin-top-3">OTHER</h2>
             </div>
 
             @else
--- poc-coder/resources/views/partials/navigation.blade.php
+++ poc-coder/resources/views/partials/navigation.blade.php
@@ -2,9 +2,9 @@
     <div class="container-fluid navbar-inverse">
         <ul class="nav navbar-nav">
             <li><a href="{{ route('welcome') }}" class="color-white hover">Home</a></li>
-            <li><a href="#statistics" class="color-white hover">#statistics</a></li>
+            <li><a href="#options" class="color-white hover">#options</a></li>
             <li><a href="#raw-content" class="color-white hover">#raw-content</a></li>
-            <li><a href="#other" class="color-white hover">#other</a></li>
+            <li><a href="#statistics" class="color-white hover">#statistics</a></li>
         </ul>
     </div>
 </nav>
