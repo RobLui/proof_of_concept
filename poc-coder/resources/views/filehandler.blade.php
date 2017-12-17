9071d619860431e8cffea467637b89d443b2d2f3
Robbert <robbertluit@hotmail.com>
2017-12-17 16:35:15 +0100

    add nav

--- poc-coder/resources/views/filehandler.blade.php
+++ poc-coder/resources/views/filehandler.blade.php
@@ -2,18 +2,21 @@
 
 @section('content')
 
+    @include('partials.navigation')
+
     <div class="col-md-12 full-display">
 
         @if(!empty($data))
+
             {{-- FILE CONTENTS --}}
-            <div class="col-md-6 full-display col-md-offset-3">
-                <h2>Inserted file raw content:</h2>
+            <div class="col-md-6 full-display col-md-offset-3" id="raw-content">
+                <h2 class="margin-top-3">Inserted file raw content:</h2>
                 <p class="margin-top-3 text-left"> {{ $data }} </p>
             </div>
 
             {{-- DETAILS FROM FILE CONTENTS --}}
-            <div class="col-md-6 col-md-offset-3 margin-bottom-3">
-                <h2>Statistics</h2>
+            <div class="col-md-6 col-md-offset-3 margin-top-3 margin-bottom-3" id="statistics">
+                <h2 class="margin-top-3">Statistics</h2>
                 <ul class="list-group margin-top-3">
                     <li class="col-md-12 list-group-item">Totaal aantal woorden: {{ $total }}</li>
                     @foreach($eachWord as $item)
@@ -21,9 +24,17 @@
                     @endforeach
                 </ul>
             </div>
+
+            {{-- OTHER --}}
+            <div class="col-md-6 full-display col-md-offset-3" id="other">
+                <h2 class="margin-top-3">Other</h2>
+            </div>
+
             @else
-            <h2>Input a file on the main page :)</h2>
-            <a href="{{ route('welcome') }}" class="btn btn-info">&larr; back to main page</a>
+            <div class="margin-top-3">
+                <h2 class="margin-top-6 margin-bottom-3">Input a file on the main page :)</h2>
+                <a href="{{ route('welcome') }}" class="btn btn-info">&larr; back to main page</a>
+            </div>
         @endif
 
     </div>
